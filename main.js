6babd21f79d49435670e4ef91720f245aef0ac82
Isabelle Moraes Rodrigues <106668424+isabellemoraes0@users.noreply.github.com>
2024-04-16 08:21:43 -0300

    i.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function verificarTime() {
     var respostaTime = prompt("Qual é a melhor serie?");
 
     
-    if (respostaTime.toLowerCase() === "You") {
+    if (respostaTime.toLowerCase() === "you") {
       alert("Isso mesmo! You é a melhor serie!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
